1e89ac37700974290b0cc76a4c285dee87ae619c
NIHAL SINGH <singh.nihal1997@gmail.com>
2021-07-29 12:18:46 +0530

    1553A_Codeforces_Code

---
+++ 1553A_Digits_Sum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t; //test case
+  cin >> t;
+  while(t--){
+    int res=0;
+    unsigned long int n;
+    cin >> n;
+    res = (n+1)/10;
+    cout << res << endl;
+  }
+  return 0;
+}
